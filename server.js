ce37d9bbf8f92d34984d129c22062390fd81d0ac
Артем Павленко <itemka@Artems-MacBook-Pro.local>
2021-01-21 17:29:40 +0300

    Add server.js for deploy to Heroku

---
+++ server.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const path = require('path');
+const compression = require('compression');
+
+dotenv.config('./env');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log('> Server started');
+});
