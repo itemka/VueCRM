f744d5cbc948202616fa1bf8fa799f714eb60d84
Артем Павленко <itemka@Artems-MacBook-Pro.local>
2021-01-24 03:54:19 +0300

    Add body-parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv'
 import express from 'express'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
 import path from 'path'
 import cors from 'cors'
 import compression from 'compression'
@@ -9,10 +10,12 @@ dotenv.config('./env')
 const PORT = process.env.PORT || 3000
 const app = express()
 
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(compression())
-app.use(cors())
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
@@ -26,4 +29,4 @@ function start() {
   })
 }
 
-start();
+start()
--- src/store/common.js
+++ src/store/common.js
@@ -13,9 +13,9 @@ export default {
   actions: {
     async fetchCurrency({ commit }) {
       try {
-        const currency = await fetch(
-          `http://data.fixer.io/api/latest?access_key=${process.env.VUE_APP_FIXER_ACCESS_KEY}&symbols=USD,EUR,BYN`
-        )
+        const proxyurl = 'https://cors-anywhere.herokuapp.com/'
+        const url = `${proxyurl}http://data.fixer.io/api/latest?access_key=${process.env.VUE_APP_FIXER_ACCESS_KEY}&symbols=USD,EUR,BYN`
+        const currency = await fetch(url)
         
         return await currency.json()
       } catch (error) {
